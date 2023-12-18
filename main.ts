0a00074acdb6f37bae01f8d17af357cd598adeb4
Anson Au <ash1712@gmail.com>
2023-12-18 18:28:31 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Yes)
     ContinuousServo.spin_one_way(AnalogPin.P1)
     ContinuousServo.spin_other_way(AnalogPin.P4)
     basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+    ContinuousServo.turn_off_motor(DigitalPin.P4)
+    ContinuousServo.turn_off_motor(DigitalPin.P1)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
 })
 basic.forever(function () {
 	
